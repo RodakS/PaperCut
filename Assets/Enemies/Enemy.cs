cb6c550aecbedadbbe0bed4cd9315f2d4ec0e096
Adrian Pogrzeba <adipoggger@wp.pl>
2020-03-21 11:37:20 +0100

    Hero and Enemy
    Dodałem klasy początkowe wroga oraz bohatera

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PaperCut
+{
+    public class Enemy : MonoBehaviour
+    {
+        public int intent = 1;
+        public int MaxHP = 80;
+        public int HP = 80;
+        public int AttackUp = 0;
+        public int Shield;
+        void Start()
+        {
+
+        }
+
+        public void substractHP(int Damage)
+        {
+            if (Damage <= Shield)
+            {
+                Shield -= Damage;
+            }
+            else
+            {
+                Damage -= Shield;
+                Shield = 0;
+                HP -= Damage;
+            }
+        }
+
+        public void Turn()
+        {
+            switch (intent)
+            {
+                case 1:
+                    //tutaj czekam aż zostanie stworzona instancja bohatera
+                   //  Hero.substractHP(6 + AttackUp);
+                    break;
+                case 2:
+                    this.Shield += 10;
+                    break;
+                case 3:
+                    this.Shield += 6;
+                    this.AttackUp += 2;
+                    break;
+
+
+            }
+            this.Shield = 0;
+            this.calculateMove();
+            //tutaj kończymy turę
+        }
+
+        public void calculateMove()
+        {
+            int AttackMove = 3;
+            int DefenseMove = 6;
+            //tutaj czekam aż zostanie stworzona instancja bohatera
+            // if (Hero.HP < Hero.HP / 2) AttackMove++;
+            if (this.HP < this.HP / 2) DefenseMove--;
+            int Decision = Random.Range(0, 11);
+            if (Decision < AttackMove)
+                intent = 1;
+            else if (Decision > DefenseMove)
+                intent = 2;
+            else intent = 3;
+        }
+         
+
+
+
+    void Update()
+        {
+
+        }
+    }
+}
+
+
---
+++ Assets/Player/Hero.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PaperCut
+{
+    public class Hero : MonoBehaviour
+    {
+        public int MaxHP = 80;
+        public int HP = 80;
+        public int AttackUp = 0;
+        public int Shield;
+        public int cardDraw = 5;
+        private const int maxCards = 10;
+        void Start()
+        {
+
+        }
+
+        public  void substractHP(int Damage)
+        {
+            if (Damage <= this.Shield)
+            {
+                this.Shield -= Damage;
+            }
+            else
+            {
+                Damage -= this.Shield;
+                this.Shield = 0;
+                this.HP -= Damage;
+            }
+        }
+
+        public void Turn()
+        {
+            
+            //tutaj mechanika zagrywania kart
+            //ciągnij cardDraw kart, ale nie więcej niż maxCards
+            //dopóki endTurn nie naciśnięte róbta rzeczy
+            //tutaj kończymy turę
+        }
+
+        
+         
+
+
+
+    void Update()
+        {
+
+        }
+    }
+}
+
+
